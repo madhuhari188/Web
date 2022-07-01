9bae79a5234f5be98ec460af29732fb226be3a4c
madhuhari188 <madhu.hari.188@gmail.com>
2022-07-01 09:23:50 +0530

    new

--- server.js
+++ server.js
@@ -33,9 +33,9 @@ app.use(cookieParser());
 app.use(bodyParser.urlencoded({
     extended: false
 }))
-app.use(express.static(path.join(__dirname, "client", "build")))
+app.use(express.static(path.join(__dirname +  `client/build`)))
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client","build", "index.html"));
+    res.sendFile(path.join(__dirname + `/client/build/index.html`));
 });
 app.use(bodyParser.json())
 app.use(passport.initialize())
