4d38c37786cead69e9e7453c251fc752d9e0384c
madhuhari188 <madhu.hari.188@gmail.com>
2022-07-01 10:45:54 +0530

    new

--- server.js
+++ server.js
@@ -56,7 +56,7 @@ app.use('/api/user',User)
 
 app.use(express.static(path.join(__dirname +  'client/build')))
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname + '/client/build","index.html'));
+    res.sendFile(path.join(__dirname + 'index.html'));
 });
 
 app.listen(port,()=>{
